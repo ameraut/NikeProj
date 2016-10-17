a7fdfb1fce8e3cd94734ed76c163447eba35398a
Jon <jonathan.james.moore@gmail.com>
2016-10-16 18:05:22 -0700

    Init Commit

---
+++ src/main/java/controller/UserService.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/user")
+public class UserService {
+
+
+    @GET
+    @Path("/{name}")
+    public String sayHello(@PathParam("name") String name) {
+        String output = "Hi from Jersey REST Service: " + name;
+        return output;
+   }
+}
