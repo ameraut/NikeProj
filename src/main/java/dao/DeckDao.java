98e98e727e22597b7c547a14abab7c4f1ccb985b
Jon <jonathan.james.moore@gmail.com>
2016-10-25 22:24:12 -0700

    Moved writeDeck from the Deck Class to the DAO

--- src/main/java/dao/DeckDao.java
+++ src/main/java/dao/DeckDao.java
@@ -66,7 +66,7 @@ public class DeckDao {
             NikeDeck deck = new NikeDeck();
             deck.setName(name);
             //Add File
-            deck.writeDeck();
+            writeDeck(deck);
         }
     }
 
@@ -84,7 +84,7 @@ public class DeckDao {
                     System.out.println(deck.getCards().toString());
                 }
                 //Update File
-                deck.writeDeck();
+                writeDeck(deck);
             }
         }
     }
@@ -106,5 +106,75 @@ public class DeckDao {
         }
     }
 
+    //Helper Method
+    private void writeDeck(NikeDeck deck){
+        ObjectMapper mapper = new ObjectMapper();
+
+        //Creating File that will be added into the DeckDirectory
+        File deckFile = new File(path +"/"+ deck.getName() + ".json");
+        ArrayList cards = new ArrayList();
+        //region Add Default cards
+        cards.add("2-heart");
+        cards.add("3-heart");
+        cards.add("4-heart");
+        cards.add("5-heart");
+        cards.add("6-heart");
+        cards.add("7-heart");
+        cards.add("8-heart");
+        cards.add("9-heart");
+        cards.add("10-heart");
+        cards.add("J-heart");
+        cards.add("Q-heart");
+        cards.add("K-heart");
+        cards.add("A-heart");
+        cards.add("2-diamond");
+        cards.add("3-diamond");
+        cards.add("4-diamond");
+        cards.add("5-diamond");
+        cards.add("6-diamond");
+        cards.add("7-diamond");
+        cards.add("8-diamond");
+        cards.add("9-diamond");
+        cards.add("10-diamond");
+        cards.add("J-diamond");
+        cards.add("Q-diamond");
+        cards.add("K-diamond");
+        cards.add("A-diamond");
+        cards.add("2-spades");
+        cards.add("3-spades");
+        cards.add("4-spades");
+        cards.add("5-spades");
+        cards.add("6-spades");
+        cards.add("7-spades");
+        cards.add("8-spades");
+        cards.add("9-spades");
+        cards.add("10-spades");
+        cards.add("J-spades");
+        cards.add("Q-spades");
+        cards.add("K-spades");
+        cards.add("A-spades");
+        cards.add("2-clubs");
+        cards.add("3-clubs");
+        cards.add("4-clubs");
+        cards.add("5-clubs");
+        cards.add("6-clubs");
+        cards.add("7-clubs");
+        cards.add("8-clubs");
+        cards.add("9-clubs");
+        cards.add("10-clubs");
+        cards.add("J-clubs");
+        cards.add("Q-clubs");
+        cards.add("K-clubs");
+        cards.add("A-clubs");
+        deck.setCards(cards);
+        //endregion
+
+        try {
+            //Writing file
+            mapper.writeValue(deckFile, deck);
+        } catch (Exception ex) {
+            System.out.println("Failure in writing the card due to the following exception: " + ex);
+        }
+    }
 
 }
--- src/main/java/resources/NikeDeck.java
+++ src/main/java/resources/NikeDeck.java
@@ -114,7 +114,7 @@ public class NikeDeck implements Shuffle {
         this.setCards(tempCards);
     }
 
-    //Helper methods
+    //Helper Shuffle method
     private ArrayList interleave() {
         //interleave cards and return merged List
         int displacement = cards.size()/2;
@@ -130,76 +130,5 @@ public class NikeDeck implements Shuffle {
             }
         }
         return tempCards;
-    }
-    public void writeDeck() {
-        ObjectMapper mapper = new ObjectMapper();
-        String path = new File("src/main/resources/DeckDirectory").getAbsolutePath();
-
-        //Creating File that will be added into the DeckDirectory
-        File deckFile = new File(path +"/"+ this.getName() + ".json");
-        ArrayList cards = new ArrayList();
-        //region Add Default cards
-        cards.add("2-heart");
-        cards.add("3-heart");
-        cards.add("4-heart");
-        cards.add("5-heart");
-        cards.add("6-heart");
-        cards.add("7-heart");
-        cards.add("8-heart");
-        cards.add("9-heart");
-        cards.add("10-heart");
-        cards.add("J-heart");
-        cards.add("Q-heart");
-        cards.add("K-heart");
-        cards.add("A-heart");
-        cards.add("2-diamond");
-        cards.add("3-diamond");
-        cards.add("4-diamond");
-        cards.add("5-diamond");
-        cards.add("6-diamond");
-        cards.add("7-diamond");
-        cards.add("8-diamond");
-        cards.add("9-diamond");
-        cards.add("10-diamond");
-        cards.add("J-diamond");
-        cards.add("Q-diamond");
-        cards.add("K-diamond");
-        cards.add("A-diamond");
-        cards.add("2-spades");
-        cards.add("3-spades");
-        cards.add("4-spades");
-        cards.add("5-spades");
-        cards.add("6-spades");
-        cards.add("7-spades");
-        cards.add("8-spades");
-        cards.add("9-spades");
-        cards.add("10-spades");
-        cards.add("J-spades");
-        cards.add("Q-spades");
-        cards.add("K-spades");
-        cards.add("A-spades");
-        cards.add("2-clubs");
-        cards.add("3-clubs");
-        cards.add("4-clubs");
-        cards.add("5-clubs");
-        cards.add("6-clubs");
-        cards.add("7-clubs");
-        cards.add("8-clubs");
-        cards.add("9-clubs");
-        cards.add("10-clubs");
-        cards.add("J-clubs");
-        cards.add("Q-clubs");
-        cards.add("K-clubs");
-        cards.add("A-clubs");
-        this.setCards(cards);
-        //endregion
-
-        try {
-            //Writing file
-            mapper.writeValue(deckFile, this);
-        } catch (Exception ex) {
-            System.out.println("Failure in writing the card due to the following exception: " + ex);
-
-        }
     }
 }
